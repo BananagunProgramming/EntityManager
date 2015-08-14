7bde99791bba810740b12c943be1a74fad4b6c92
bananagun <bananagun@live.com>
2015-08-13 20:48:21 -0400

    Add last updated and last updated by property to base domain codefirst class

--- EntityManager/EntityManager.Domain/CodeFirst/DomainBase.cs
+++ EntityManager/EntityManager.Domain/CodeFirst/DomainBase.cs
@@ -7,5 +7,7 @@ namespace EntityManager.Domain.CodeFirst
         public bool IsDeleted { get; set; }
         public DateTime CreatedOn { get; set; }
         public string CreatedBy { get; set; }
-    }
+        public DateTime LastUpdateDate { get; set; }
+        public string LastUpdatedBy { get; set; }
+}
 }

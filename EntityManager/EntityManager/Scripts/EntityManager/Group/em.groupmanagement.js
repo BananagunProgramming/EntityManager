92591ba47c74f832f6eb46bd1a49aa021683ca53
bananagun <bananagun@live.com>
2015-08-23 18:56:44 -0400

    Simpler implementation of the revealing module pattern for jQuery inclusion. Will get this reviewed by an expert.

--- EntityManager/EntityManager/Scripts/EntityManager/Group/em.groupmanagement.js
+++ EntityManager/EntityManager/Scripts/EntityManager/Group/em.groupmanagement.js
@@ -1,53 +1,54 @@
-﻿var entitymanager;
-(function (entitymanager) {
-    (function (groupmanagement) {
-        //var settings = {};
+﻿var GroupManagement = function () {
 
-        var GroupManagement = (function () {
+    init = function () {
+        editGroup();
+        cancel();
+        saveGroup();
+    };
 
-            function GroupManagement() {
-                //$.extend(true, settings, opts);
-                //var self = this;
-            }
+    editGroup = function () {
+        $('#group-edit').on('click', function () {
+            toggleGroupManage();
+            return false;
+        });
+    }
 
-            GroupManagement.prototype.init = function () {
-                var _this = this;
+    cancel = function () {
+        $('#group-cancel').on('click', function () {
+            toggleGroupManage();
+        });
+    }
 
-                $('#group-edit').on('click', function () {
-                    toggleGroupManage();
-                    return false;
-                });
+    saveGroup = function () {
+        $('#group-save').on('click', function () {
+            var _this = this;
+            var url = "/Group/ManageGeneral/";
+            //var postData = AddAntiForgeryToken() +'&' + $('#generalform').serialize();
+            var postData = $('#generalform').serialize();
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: postData,
+                success: function (result) {
+                    _this.$section = $(result).replaceAll($('#generalform'));
+                    init();
+                },
+            });
+        });
+    }
+    
+    toggleGroupManage = function () {
+        $('#manage-general').toggle();
+        $('#manage-general-edit').toggle();
+    };
+    
+    return {
+        init: init,
+        editGroup: editGroup,
+        cancel: cancel,
+        saveGroup: saveGroup,
+        toggleGroupManage: toggleGroupManage
+    };
+}();
 
-                $('#group-cancel').on('click', function () {
-                    toggleGroupManage();
-                });
-
-                $('#group-save').on('click', function() {
-                    var _this = this;
-                    var url = "/Group/ManageGeneral/";
-                    //var postData = AddAntiForgeryToken() +'&' + $('#generalform').serialize();
-                    var postData = $('#generalform').serialize();
-                        $.ajax({
-                            type: "POST",
-                            url: url,
-                            data: postData,
-                        success: function (result) {
-                            _this.$section = $(result).replaceAll($('#generalform'));
-                            GroupManagement.prototype.init();
-                        },
-                    });
-                });
-
-                //called methods
-                function toggleGroupManage() {
-                    $('#manage-general').toggle();
-                    $('#manage-general-edit').toggle();
-                };
-            };
-
-            return GroupManagement;
-        })();
-        groupmanagement.GroupManagement = GroupManagement;
-    })(entitymanager.groupmanagement || (entitymanager.groupmanagement = {}));
-})(entitymanager || (entitymanager = {}));
 
